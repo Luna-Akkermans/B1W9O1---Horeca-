c05404e7ac813c20d528856041533ad17fac60f0
Luna Mila <9904466@mydavinci.nl>
2021-11-22 14:27:10 +0100

    fixed-code-oop

--- products.js
+++ products.js
@@ -1,32 +1,54 @@
-var invoice = []
-var drinks = [{
+var Products = [{
     name: "Fristi",
     price: 1.10,
-    amount: 0,
-},{
+}, {
     name: "Bier",
-    price: 2.30,
-    amount: 0
-},{
+    price: 1.5
+}, {
     name: "Fris",
     price: 1.80,
-    amount: 0
+},
+{
+    name: 'Snacks(8,16)',
+    type: [{
+        amountOfSnacks: 8,
+        price: 4.20,
+    }, {
+        amountOfSnacks: 16,
+        price: 8,
+    }]
 }]
 
-var snacks = [{
-    name: "8",
-    price: 4.20,
-    amount: 0
-},{
-    name: "16",
-    price: 8,
-    amount: 0
-}]
+var cart = [{
+        name: 'Fristi',
+        totalPrice: 0,
+    },
+    {
+        name: 'Bier',
+        totalPrice: 0
+    }, {
+        name: 'Fris',
+        totalPrice: 0
+    },
+    {
+        name: 'Snacks',
+        type: [{
+            name: '8 Snacks',
+            amountOfSnacks: 8,
+            totalPrice: 0
+        }, {
+            name: '16 Snacks',
+            amountOfSnacks: 16,
+            totalPrice: 0,
+        }]
+    }
+];
 
 
 
-function invoiceCreation(name, amount,price){
+
+function invoiceCreation(name, amount, price) {
     this.name = name;
     this.amount = amount;
-    this.total = '€' + amount*price;
+    this.total = '€' + amount * price;
 }
--- script.js
+++ script.js
@@ -1,51 +1,71 @@
-const drinkNames = drinks.map(key => key.name);
-const resultLi = document.createElement('li');
+//Order Status
 var orderStatus = true;
-var orderQuestion = (typeArray, index, nameOfItem) => {
-    typeArray[index].amount = Number(prompt(`Vul aantal ${nameOfItem} in, wilt u toch geen ${nameOfItem} laat het veld leeg en druk oké.`));
-}
-
-var calcuLateCart = (arrayToCalc) => {
-    arrayToCalc.forEach(function (key) {
-        if (key.amount != 0 && !isNaN(key.amount)) {
-            invoice[key.name] = new invoiceCreation(key.name, key.amount, key.price);
-        }
-    })
-
-    for (key in invoice) {
-        resultLi.innerHTML = `<span>${invoice[key].name} </span> <span>${invoice[key].amount} </span> <span>${invoice[key].total} </span>`
-        if(invoice.length >= 0){
-         rekening.appendChild(resultLi)
+var totalOrderPrice = 0;
+//Generate user qestion.
+const orderQuestion = (input) => {
+    cartIndex = cart.findIndex(item => item.name === input);
+    if(input != 'Snacks'){
+        cart[cartIndex].amount = Number(prompt(`Vul aantal ${cart[cartIndex].name} in, wilt u toch geen ${cart[cartIndex].name} laat het veld leeg en druk oké.`));
+    }else{
+        amountOfSnacks = Number(prompt("Wilt u 8 of 16 snacks?"));
+        if(amountOfSnacks == 8){
+            cart[cartIndex].type[0].amount = Number(prompt(`Vul aantal ${cart[cartIndex].type[0].name} in, wilt u toch geen ${cart[cartIndex].type[0].name} laat het veld leeg en druk oké.`));
+        }else if(amountOfSnacks == 16){
+            cart[cartIndex].type[1].amount = Number(prompt(`Vul aantal ${cart[cartIndex].type[1].name} in, wilt u toch geen ${cart[cartIndex].type[1].name} laat het veld leeg en druk oké.`));
         }
     }
+  
+}  
+
+const finalizeOrder = () => {
+  
+    orderStatus = false;
+    for(var key in cart){
+        if(cart[key].hasOwnProperty("amount")){
+            console.log(key)
+            cart[key].totalPrice = cart[key].amount * Products[key].price
+            totalOrderPrice = totalOrderPrice + cart[key].totalPrice;
+            var li = document.createElement("li");
+            li.innerHTML = `<span>${cart[key].name} </span> <span>${cart[key].amount}</span>  <span>€ ${cart[key].totalPrice}.-</span> `
+            rekening.appendChild(li)
+        }
+        if(cart[key].hasOwnProperty("type")){
+            for(var index in cart[key].type){
+                if(cart[key].type[index].hasOwnProperty("amount")){
+                    cart[key].type[index].totalPrice = cart[key].type[index].amount * Products[key].type[index].price
+                    totalOrderPrice = totalOrderPrice + cart[key].type[index].totalPrice;
+                    var li = document.createElement("li");
+                    li.innerHTML = `<span>${cart[key].type[index].name} </span> <span>${cart[key].type[index].amount}</span>  <span>€ ${cart[key].type[index].totalPrice}.-</span> `
+                    rekening.appendChild(li)
+                }
+            }
+        }
+    }
+    var span = document.createElement("span");
+    span.innerHTML = `€ ${totalOrderPrice}.-`
+    totaal.appendChild(span)
 }
+
+
 do {
-    switch (prompt("Wat wil jij bestellen? \n" + drinkNames + " Snacks").toLowerCase()) {
+    switch (prompt("Wat wilt u bestellen? " + Products.map(key => " " + key.name)).toLowerCase()) {
         case "fristi":
-            orderQuestion(drinks, 0, "fristi")
+            orderQuestion('Fristi');
             break;
         case "bier":
-            orderQuestion(drinks, 1, "bier")
+            orderQuestion('Bier');
             break;
         case "fris":
-            orderQuestion(drinks, 2, "fris")
+            orderQuestion('Fris');
             break;
         case "snacks":
-            switch(prompt("Hoeveel wilt u? \n 8 of 16").toLowerCase()){
-                case "8":
-                    orderQuestion(snacks, 0, "8 Bitterballen")
-                    break
-                case "16":
-                    orderQuestion(snacks, 1, "16 Bitterballen")
-                    break;
-            }
-            break;  
-        case "stop":
-            calcuLateCart(drinks);
-            calcuLateCart(snacks);
-            orderStatus = false;
+            orderQuestion('Snacks');
             break;
+        case "stop":
+            finalizeOrder();
+            break;
+        default:
+            break;
+
     }
 } while (orderStatus)
-
-
